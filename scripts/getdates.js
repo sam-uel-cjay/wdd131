d60b3149ab16b83a0f2b44db50b521fcab67e8d1
Your Name <ngozichijike@gmail.com>
2025-09-06 18:28:31 +0100

    index.html

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+const currentYear = document.getElementById("currentyear");
+const lastModified = document.getElementById("lastModified");
+
+currentYear.textContent = new Date().getFullYear();
+lastModified.textContent = `Last Modified: ${document.lastModified}`;
