59257d3d0b4a1fd1f20f2eb9c7f3cfcba57fc94c
Your Name <ngozichijike@gmail.com>
2025-09-13 11:25:46 +0100

    temples.html

---
+++ temples/temples.js
@@ -0,0 +1,9 @@
+document.getElementById("year").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = document.lastModified;
+
+const menuButton = document.querySelector("#menu");
+const navigation = document.querySelector("nav");
+
+menuButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+});
